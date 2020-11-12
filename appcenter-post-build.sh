5d2a800233a34687a2243d59659f1b667fbb2cf2
YonglianQian <qianyonglian@outlook.com>
2020-11-12 11:35:30 +0800

    Update appcenter-post-build.sh

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,25 +1,19 @@
 echo "Found NUnit test projects:"
 find $APPCENTER_SOURCE_DIRECTORY -regex '.*Test.*\.csproj' -exec echo {} \;
-
-echo "------------"
+echo
 echo "Building NUnit test projects:"
 find $APPCENTER_SOURCE_DIRECTORY -regex '.*Test.*\.csproj' -exec msbuild {} \;
-
-echo "------------"
+echo
 echo "Compiled projects to run NUnit tests:"
 find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*Test.*\.dll' -exec echo {} \;
-
-echo "------------"
+echo
 echo "Running NUnit tests:"
 find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*Test.*\.dll' -exec nunit3-console {} +
-
-echo "------------"
+echo
 echo "NUnit tests result:"
 pathOfTestResults=$(find $APPCENTER_SOURCE_DIRECTORY -name 'TestResult.xml')
 cat $pathOfTestResults
-
-echo "-------------"
-
+echo
 #look for a failing test
 grep -q 'result="Failed"' $pathOfTestResults
 
